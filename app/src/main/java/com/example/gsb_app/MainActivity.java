169780c00b0ae8fb62f5bebfecfd99f59b5e9e30
EdouardQ <edouard.quilliou@gmail.com>
2021-03-10 01:55:57 +0100

    commit 0.01
    ajout gsb_loho.png
    ajout activity_main.xml + secret edt_password depuis le mainactivity.java

--- app/src/main/java/com/example/gsb_app/MainActivity.java
+++ app/src/main/java/com/example/gsb_app/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.gsb_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edt_loging;
+    private EditText edt_password;
+    private Button btn_connexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edt_password = (EditText) findViewById(R.id.edt_password);
+        edt_password.setTransformationMethod(new PasswordTransformationMethod());
     }
 }
