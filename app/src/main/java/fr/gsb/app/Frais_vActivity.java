98a401c4fd3de4b4b5a4485000c977c29d976400
devFendrix <dev.fendrix@gmail.com>
2021-04-11 17:58:04 +0200

    Commit 0.09
    - Merge prod to dev

---
+++ app/src/main/java/fr/gsb/app/Frais_vActivity.java
@@ -0,0 +1,75 @@
+package com.example.gsb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Frais_vActivity extends AppCompatActivity {
+
+    private Button btn_dcnx;
+    private Button btn_praticien;
+    private Button btn_rdv;
+    private Button btn_frais;
+    private Button btn_profil;
+    private TextView tv_ident;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frais_v);
+
+        btn_dcnx = findViewById(R.id.deconnexion);
+        btn_praticien = findViewById(R.id.praticien);
+        btn_rdv = findViewById(R.id.rdv);
+        btn_frais = findViewById(R.id.frais);
+        btn_profil = findViewById(R.id.profil);
+        tv_ident = findViewById(R.id.tv_ident);
+
+         Intent i_recu = getIntent();
+         String ident_recu = i_recu.getStringExtra("ident");
+         tv_ident.setText(ident_recu);
+
+        btn_dcnx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent connexion = new Intent(Frais_vActivity.this, MainActivity.class);
+                startActivity(connexion);
+            }
+        });
+        btn_praticien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String valeur = tv_ident.getText().toString();
+                Intent praticien = new Intent(Frais_vActivity.this, PraticienActivity.class);
+                praticien.putExtra("ident", valeur);
+                startActivity(praticien);
+            }
+        });
+
+        btn_rdv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String valeur = tv_ident.getText().toString();
+                Intent rdv = new Intent(Frais_vActivity.this, RdvActivity.class);
+                rdv.putExtra("ident", valeur);
+                startActivity(rdv);
+            }
+        });
+        btn_profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String valeur = tv_ident.getText().toString();
+                Intent profil = new Intent(Frais_vActivity.this, ProfilActivity.class);
+                profil.putExtra("ident", valeur);
+                startActivity(profil);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/fr/gsb/app/Menu_comptableActivity.java
@@ -0,0 +1,63 @@
+package com.example.gsb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Menu_comptableActivity extends AppCompatActivity {
+
+    private Button btn_dcnx;
+    private Button btn_val_frais;
+    private Button btn_frais_mois;
+    private Button btn_profil;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_comptable);
+
+        btn_dcnx = findViewById(R.id.deconnexion);
+        btn_val_frais = findViewById(R.id.validation_frais);
+        btn_frais_mois = findViewById(R.id.frais_mois);
+        btn_profil = findViewById(R.id.profil_compt);
+
+        btn_dcnx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent connexion = new Intent(Menu_comptableActivity.this, MainActivity.class);
+                startActivity(connexion);
+            }
+        });
+
+        btn_val_frais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent val_frais = new Intent(Menu_comptableActivity.this, Frais_comptActivity.class);
+                startActivity(val_frais);
+            }
+        });
+
+        btn_frais_mois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent frais_mois = new Intent(Menu_comptableActivity.this, Frais_moisActivity.class);
+                startActivity(frais_mois);
+            }
+        });
+
+        btn_profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profil = new Intent(Menu_comptableActivity.this, Profil_comptActivity.class);
+                startActivity(profil);
+            }
+        });
+    }
+}
