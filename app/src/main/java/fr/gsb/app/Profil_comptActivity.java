96d5b1a927acaa5fd1532576d7a61eb48f31f73e
Jogroe <56687596+Jogroe@users.noreply.github.com>
2021-04-11 16:18:47 +0200

    commit 0.09
    layout et java pages

---
+++ app/src/main/java/fr/gsb/app/Profil_comptActivity.java
@@ -0,0 +1,54 @@
+package com.example.gsb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Profil_comptActivity extends AppCompatActivity {
+
+    private Button btn_dcnx;
+    private Button btn_val_frais;
+    private Button btn_frais_mois;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_compt);
+
+        btn_dcnx = findViewById(R.id.deconnexion);
+        btn_val_frais = findViewById(R.id.validation_frais);
+        btn_frais_mois = findViewById(R.id.frais_mois);
+
+        btn_dcnx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent connexion = new Intent(Profil_comptActivity.this, MainActivity.class);
+                startActivity(connexion);
+            }
+        });
+
+        btn_val_frais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent val_frais = new Intent(Profil_comptActivity.this, Frais_comptActivity.class);
+                startActivity(val_frais);
+            }
+        });
+
+        btn_frais_mois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent frais_mois = new Intent(Profil_comptActivity.this, Frais_moisActivity.class);
+                startActivity(frais_mois);
+            }
+        });
+
+
+    }
+}
