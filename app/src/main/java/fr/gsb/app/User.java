c51df15023ac346a674506610ae06c0190e2e8a2
EdouardQ <edouard.quilliou@gmail.com>
2021-05-24 22:13:29 +0200

    commit 0.42
    update User.java + visitor_profil.xml

--- app/src/main/java/fr/gsb/app/User.java
+++ app/src/main/java/fr/gsb/app/User.java
@@ -6,15 +6,21 @@ import java.io.Serializable;
 
 public class User implements Serializable {
     private String id;
+    private String password;
     private String name;
     private String firstName;
     private String role;
+    private String phone;
+
+    public User() {
+    }
 
     public User(DocumentSnapshot document) {
         this.id = document.getId();
         this.name = document.getString("name");
         this.firstName = document.getString("firstName");
         this.role = document.getString("role");
+        this.phone = document.getString("phone");
     }
 
     public String getName() {
@@ -47,5 +53,21 @@ public class User implements Serializable {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
