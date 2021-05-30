8ff1e1e3271d1142458ecdf75b4d736186f10452
devFendrix <dev.fendrix@gmail.com>
2021-05-30 16:54:01 +0200

    Commit 0.49
    Ajout du choix de l'heure dans l'ajout d'un rdv

--- app/src/main/java/fr/gsb/app/VisitorCalendarAddRdvActivity.java
+++ app/src/main/java/fr/gsb/app/VisitorCalendarAddRdvActivity.java
@@ -1,11 +1,13 @@
 package fr.gsb.app;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,22 +15,22 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class VisitorCalendarAddRdvActivity extends AppCompatActivity {
-    private EditText etDate;
-    private Button btnAddRdv;
+    private EditText etDate, etTime;
+    private Button btnAddDate, btnAddTime;
 
-    private int lastSelectedYear;
-    private int lastSelectedMonth;
-    private int lastSelectedDayOfMonth;
+    private int lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth;
+    private int lastSelectedHour = -1, lastSelectedMinute = -1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visitor_calendar_add_rdv);
 
+        // Partie sur la sélection de la date (Jour/Mois/Année)
         this.etDate = this.findViewById(R.id.et_date);
-        this.btnAddRdv = this.findViewById(R.id.btn_add_rdv);
+        this.btnAddDate = this.findViewById(R.id.btn_add_date);
 
-        this.btnAddRdv.setOnClickListener(new View.OnClickListener() {
+        this.btnAddDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 buttonSelectDate();
@@ -39,15 +41,24 @@ public class VisitorCalendarAddRdvActivity extends AppCompatActivity {
         this.lastSelectedYear = c.get(Calendar.YEAR);
         this.lastSelectedMonth = c.get(Calendar.MONTH);
         this.lastSelectedDayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+
+        // Partie sur la sélection de l'heure
+        this.etTime = this.findViewById(R.id.et_time);
+        this.btnAddTime = this.findViewById(R.id.btn_add_time);
+
+        this.btnAddTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonSelectTime();
+            }
+        });
     }
 
     private void buttonSelectDate() {
         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
 
             @Override
-            public void onDateSet(DatePicker view, int year,
-                                  int monthOfYear, int dayOfMonth) {
-
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 etDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
 
                 lastSelectedYear = year;
@@ -59,6 +70,27 @@ public class VisitorCalendarAddRdvActivity extends AppCompatActivity {
         DatePickerDialog datePickerDialog = null;
         datePickerDialog = new DatePickerDialog(this, dateSetListener, lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth);
         datePickerDialog.show();
+    }
+
+    private void buttonSelectTime() {
+        if(this.lastSelectedHour == -1) {
+            final Calendar c = Calendar.getInstance();
+            this.lastSelectedHour = c.get(Calendar.HOUR_OF_DAY);
+            this.lastSelectedMinute = c.get(Calendar.MINUTE);
+        }
+
+        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                etTime.setText(hourOfDay + ":" + minute);
+                lastSelectedHour = hourOfDay;
+                lastSelectedMinute = minute;
+            }
+        };
+
+        TimePickerDialog timePickerDialog = null;
+        timePickerDialog = new TimePickerDialog(this, timeSetListener, lastSelectedHour, lastSelectedMinute, true);
+        timePickerDialog.show();
     }
 
 
