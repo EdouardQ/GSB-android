bc7fefbccbb29503a64873c71355b03e0d2a57a1
Jogroe <56687596+Jogroe@users.noreply.github.com>
2021-05-30 18:02:57 +0200

    Commit 0.52
    Liste des fiches de frais comptable

--- app/src/main/java/fr/gsb/app/AccountantListExpenseFormLeftActivity.java
+++ app/src/main/java/fr/gsb/app/AccountantListExpenseFormLeftActivity.java
@@ -2,13 +2,25 @@ package fr.gsb.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountantListExpenseFormLeftActivity extends AppCompatActivity {
 
@@ -16,6 +28,8 @@ public class AccountantListExpenseFormLeftActivity extends AppCompatActivity {
     private Button btn_frais_mois;
     private Button btn_profil;
     private TextView tv_ident;
+    private ListView lv_expenseform;
+    private List<ExpenseForm> expenseformList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,42 @@ public class AccountantListExpenseFormLeftActivity extends AppCompatActivity {
         btn_frais_mois = findViewById(R.id.frais_mois);
         btn_profil = findViewById(R.id.profil_compt);
         tv_ident = findViewById(R.id.tv_ident);
+        lv_expenseform = findViewById(R.id.lv_expenseform);
+
+
+// listage des fiches de frais non validées
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        expenseformList = new ArrayList<>();
+
+
+        db.collection("expense_forms")
+                .get() // récupère tout les fiches de frais de la collection
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //Log.d("FIREC", document.getId() + " => " + document.getData());
+                                ExpenseForm tmp_expenseform = new ExpenseForm(document);
+                                if (tmp_expenseform.getState().getWording().equals("Saisie clôturée")) {
+                                    expenseformList.add(tmp_expenseform);
+                                }
+
+
+                            }
+                            // ajoute la liste des fiches frais dans la listview
+                            ExpenseFormAdapter expenseAdapt = new ExpenseFormAdapter(AccountantListExpenseFormLeftActivity.this, expenseformList);
+                            lv_expenseform.setAdapter(expenseAdapt); // affiche la listview
+                        } else {
+                            Log.d("FIREC", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+
 
         //récupère les infos pour le nom prénom
         Intent i_recu = getIntent();
